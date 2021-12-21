0852887e2b6bd093d483ad9b1ac233b12b9bb715
Serhii Pylypenko <jpee.dev@gmail.com>
2021-12-21 11:40:24 +0200

    develop fix(storage) rm test value from localStorage

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,7 +1,9 @@
 const isStorageAvailable = function() {
   try {
     localStorage.setItem('key', 'value');
-    return localStorage.getItem('key') === 'value';
+    const isAvailable = localStorage.getItem('key') === 'value';
+    localStorage.removeItem('key');
+    return isAvailable;
   } catch (err) {
     console.error('LocalStorage is not available -> ' + err.message);
     return false;
